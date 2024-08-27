ed42b26d9a466d978d39bd66a1eeaa8d4d0b3119
mayuri khade <mayurikhade85@gmail.com>
2024-08-27 13:11:51 +0530

    initial  setup

--- model/Todo.js
+++ model/Todo.js
@@ -3,8 +3,8 @@ const mongoose = require("mongoose")
 const todoSchema = new mongoose.Schema({
     task: { type: String, required: true },
     desc: { type: String, required: true },
-    priority: { type: Boolean, required: true },
-    complete: { type: Boolean, default: false },
+    priority: { type: String, required: true },
+    complete: { type: Boolean, required: false },
 }, { timestamps: true })
 
 module.exports = mongoose.model("todo", todoSchema)
