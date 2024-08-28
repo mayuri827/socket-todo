b9990ed57667e4999aaea2789fe4e1ad043d40c3
mayuri khade <mayurikhade85@gmail.com>
2024-08-28 14:07:53 +0530

    404 fix

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require("cors")
 const mongoose = require("mongoose")
 const { app, httpServer } = require("./socket/socket")
 require("dotenv").config()
+const path = require("path")
 
 // const app = express()
 
@@ -19,7 +20,9 @@ app.use("/api/notes", require("./routers/todo.router"))
 
 //step 3 404 routes
 app.use("*", (req, res) => {
-    res.status(404).json({ message: "Resource Not Found 404" }) //route ani method check kraychi error aalatr
+    //                      👇Absolute path
+    res.sendFile(path.join(__dirname, "dist", "index.html"))
+    // res.status(404).json({ message: "Resource Not Found 404" }) //route ani method check kraychi error aalatr
 })
 //step 4 error handler
 app.use((err, req, res, next) => {
